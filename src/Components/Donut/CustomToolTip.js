ef63cd2b9fa0e0563b67997507d7b2d0e672664c
JH-Jitu <jitu000166@gmail.com>
2020-12-05 18:24:32 +0600

    chard done

---
+++ src/Components/Donut/CustomToolTip.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CustomToolTip = () => {
+    return (
+        <div>
+            const CustomTooltip = ({ active, payload, label }) => {
+  if (active) {
+    return (
+      <div className="custom-tooltip">
+        <p className="label">{`${label} : ${payload[0].value}`}</p>
+        <p className="intro">{getIntroOfPage(label)}</p>
+        <p className="desc">Anything you want can be displayed here.</p>
+      </div>
+    );
+  }
+        </div>
+    );
+};
+
+export default CustomToolTip;
