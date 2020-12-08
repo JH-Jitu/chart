232fe3e173103ccb2996b1a7b8e5a404efad56d6
JH-Jitu <jitu000166@gmail.com>
2020-12-08 14:56:32 +0600

    navbar bugs fixed by table method

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,37 +5,20 @@ import { faBug, faChartLine, faCodeBranch, faServer} from '@fortawesome/free-sol
 
 const Navbar = () => {
     return (
-        <div className="container">
-            <nav className="navbar mainNav navbar-expand-lg">
-  <div className="container-fluid">
-    <a className="text-dark logo navbar-brand" href="#"><img src="https://png.pngtree.com/templates/sm/20180616/sm_5b2470f712e9a.jpg" alt=""/></a>
-    <button className="navbar-toggler btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon">Menu</span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-      <li className="nav-item">
-          <a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faServer} />Data Library</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCodeBranch} />Workflows</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#b"><FontAwesomeIcon className="icon" icon={faPlayCircle} />Scheduler</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faBug} />Error Manager</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCommentDots} />Notifications</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faChartLine} />Reports</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div className="container mainNav">
+          <div style={{overflowX:"auto"}}>
+                                <table class="parent">
+                                    <tr>
+                                    <td class="avatar"><a className="logo" href="#"><img src="https://png.pngtree.com/templates/sm/20180616/sm_5b2470f712e9a.jpg" alt=""/></a></td>
+                                        <td class="avatar"><a className="nav-link mr-2" href="#c"><FontAwesomeIcon className="icon" icon={faServer} />Data Library</a></td>
+                                        <td class="avatar"><a className="nav-link mr-2" href="#c"><FontAwesomeIcon className="icon" icon={faCodeBranch} />Workflows</a></td>
+                                        <td class="avatar"><a className="nav-link mr-2" href="#b"><FontAwesomeIcon className="icon" icon={faPlayCircle} />Scheduler</a></td>
+                                        <td class="avatar"><a className="nav-link mr-2" href="#c"><FontAwesomeIcon className="icon" icon={faBug} />Error Manager</a></td>
+                                        <td class="avatar"><a className="nav-link mr-2" href="#c"><FontAwesomeIcon className="icon" icon={faCommentDots} />Notifications</a></td>
+                                        <td class="avatar"><a className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faChartLine} />Reports</a></td>
+                                    </tr>
+                                </table>
+                            </div>
         </div>
     );
 };
--- src/Components/Navbar/StatsBar.js
+++ src/Components/Navbar/StatsBar.js
@@ -4,37 +4,27 @@ import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 const StatsBar = () => {
     return (
-        <div>
-            <nav className="w-100 navbar statsBar navbar-expand-lg">
-  <div className="container-fluid">
-    <div align="left"><a className="text-dark navbar-brand" href="#">Data Statistics</a></div>
-    <button className="navbar-toggler btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon">Menu</span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav ml-auto">
-      <li className="nav-item">
-          <a style={{color: 'rgb(57, 113, 216)'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCircle} />Incoming Data</a>
-        </li>
-        <li className="nav-item">
-          <a style={{color: 'rgb(101, 166, 240)'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCircle} />No of Error detected</a>
-        </li>
-        <li className="nav-item">
-          <a style={{color: 'grey'}} className="nav-link" href="#b"><FontAwesomeIcon className="icon" icon={faCircle} />No of error fixed number</a>
-        </li>
-        <li className="nav-item" >
-          <a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCircle} />Number of error unassigned</a>
-        </li>
-        <li className="nav-item">
-          <a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCircle} />Number of error unassigned</a>
-        </li>
-        <li className="nav-item">
-          <a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon" icon={faCircle} />Menu▽</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        <div class="statsBar">
+          <div style={{overflowX:"auto"}}>
+                                <div className="d-flex justify-content-between">
+                                <table class="parent">
+                                    <tr>
+                                    <td class="avatar"><b>Data Statistics</b></td>
+                                       
+                                    </tr>
+                                </table>
+                                <table class="parent">
+                                    <tr>
+                                        <td class="avatar"><a style={{color: 'rgb(57, 113, 216)'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />Incoming Data</a></td>
+                                        <td class="avatar"><a style={{color: 'rgb(101, 166, 240)'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />No of Error detected</a></td>
+                                        <td class="avatar"><a style={{color: 'grey'}} className="nav-link" href="#b"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />No of error fixed number</a></td>
+                                        <td class="avatar"><a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />Number of error unassigned</a></td>
+                                        <td class="avatar"><a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />Number of error unassigned</a></td>
+                                        <td class="avatar"><a style={{color: 'grey'}} className="nav-link" href="#c"><FontAwesomeIcon className="icon mr-1" icon={faCircle} />Menu▽</a></td>
+                                    </tr>
+                                </table>
+                                </div>
+                            </div>
         </div>
     );
 };
